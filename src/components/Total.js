a415133a92b9562658c97fc29a71df927b955cc5
BenHall-7 <benjaminjahall@gmail.com>
2020-02-05 05:09:54 -0500

    Update Total with added features

--- src/components/Total.js
+++ src/components/Total.js
@@ -4,7 +4,10 @@ import {connect} from 'react-redux';
 const Total = props => {
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${
+        props.car.features.reduce((sum, feat) => sum + feat.price, 0)
+        + props.car.price
+      }</h4>
     </div>
   );
 };
